0ad05a30f6be57d6ff777c399bf1aad1df80dd23
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-08-15 15:04:42 +0530

    Updated the backend model

--- backend/models/Applicant.js
+++ backend/models/Applicant.js
@@ -2,11 +2,15 @@ import mongoose from "mongoose";
 
 const applicantSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String, required: true },
-  department: { type: String, required: true },
-  skills: { type: String },
-  message: { type: String },
+  srmEmail: { type: String, required: true, unique: true },
+  personalEmail: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  yearOfStudy: { type: String, required: true },
+  course: { type: String, required: true },
+  specialization: { type: String, required: true },
+  regNo: { type: String, required: true, unique: true },
+  domain: { type: String, required: true },
+  motivation: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
